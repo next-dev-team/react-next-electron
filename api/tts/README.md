a28167f5caefc72c375758c5ad50050577f2034b
Sila Rim <51617383+rimsila@users.noreply.github.com>
2024-11-23 13:37:03 +0700

    update

---
+++ api/tts/README.md
@@ -0,0 +1,2 @@
+# https://github.com/KenMwaura1/Fast-Api-example
+# https://github.com/equinor/template-fastapi-react/tree/main/api/src
--- api/tts/main.py
+++ api/tts/main.py
@@ -1,3 +1,5 @@
+
+
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, Field
 import uvicorn
@@ -7,7 +9,7 @@ from loguru import logger
 from modules.tts import tts_engine
 
 
-app = FastAPI()
+app =  FastAPI(swagger_ui_parameters={})
 
 origins = ["*"]
 
@@ -33,6 +35,10 @@ def ping():
         return {"text": result}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+@app.get("/")
+def read_root():
+ return {"Hello": "World"}
 
 
 if __name__ == "__main__":
